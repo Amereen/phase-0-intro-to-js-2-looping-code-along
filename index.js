370e9aa6485ad987b2b5f13f05aa94e0a3075e7c
Amereen Hanvel <amereenhanvel786@gmail.com>
2022-08-31 20:29:40 -0400

    commit tree 5f3a920f853fb9ee6ad877dd21eac0de79078fa0

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+// Code your solutions in this filefir 
+
+// for (let age = 30; age < 40; age ++){
+//   console.log (`I'm ${age} years old. Happy birthday to me`);
+//   debugger;
+// }
+
+const gifts= ["teddy bear", "drone","doll"];
+
+function wrapGifts(gifts){
+  for (let i= 0; i < gifts.length; i++){
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+  return gifts;
+}
+wrapGifts(gifts);
+
+const  myArray= ["Cat","Rabbit","horse"]
+const myEvent= "Surprise"
+
+function writeCards(arrayOfStrings,eventName){
+  let myArray=[]
+  for (let i= 0; i < arrayOfStrings.length; i++){
+  myArray.push(`Thank you, ${arrayOfStrings[i]}, for the wonderful surprise gift!`)
+
+}
+return myArray
+}
+writeCards(myArray,myEvent)
+
+function countDown(number){
+  while (number >= 0){
+    console.log(number)
+number --
+  }
+
+}
